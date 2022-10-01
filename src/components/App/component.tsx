ec45db1afc1e741306511ce730592f1a199a9ddc
john-mirage <chaosdynamix@posteo.net>
2022-10-01 05:51:29 +0200

    feat: set fonts and colors

--- src/components/App/component.tsx
+++ src/components/App/component.tsx
@@ -2,8 +2,10 @@ import { FunctionComponent } from "react";
 
 const App: FunctionComponent = () => {
   return (
-    <div></div>
+    <>
+      
+    </>
   );
-};
+}
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,12 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/fm-space-tourism-multi-page-website/",
-  plugins: [react({
-    babel: {
-      plugins: [
-        ["babel-plugin-styled-components", { "fileName": false }]
-      ]
-    }
-  })],
+  plugins: [react()],
   resolve: {
     alias: {
       "@components": resolve(__dirname, "src/components"),
       "@images": resolve(__dirname, "src/images"),
+      "@styles": resolve(__dirname, "src/styles"),
     },
   },
 });
